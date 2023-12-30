21015392c17b461c3c5de8f39d40055fe08e7afb
milanfin <milan.makipaa@gmail.com>
2023-12-30 11:17:56 +0200

    attempting to disable entire route cache to avoid serving stale components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ import {
 import LineChart from "@/components/linechart";
 import News from "@/components/news";
 
+export const fetchCache = 'force-no-store';
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
     const yleArticles = await getYleContent();
     const hsArticles = await getHsContent();
