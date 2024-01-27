51f2ceb1a37dd9fdb14ca4f613e5f9b670d7904b
milanfin <milan.makipaa@gmail.com>
2024-01-27 15:57:55 +0200

    fixed missing timeout param

--- src/pages/api/price.tsx
+++ src/pages/api/price.tsx
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 var convert = require("xml-js");
 
 const price = async (req: NextApiRequest, res: NextApiResponse) => {
-    let timeData = await getDayAheadData(true);
+    let timeData = await getDayAheadData(true, 9500);
     res.status(200).json(timeData);
 };
 
